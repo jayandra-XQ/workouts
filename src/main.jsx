9ef22449c688a344e00f0d36c6251e91c12eac25
Jayandra Hamal <salonhamal278@gmail.com>
2023-12-26 22:51:27 +0530

    Ready to deploy

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+import { WorkoutsContextProvider } from './context/WorkoutsContext.jsx';
+
+if (Process.env.NODE_ENV === 'production') disableReactDevTools()
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <WorkoutsContextProvider>
+      <App />
+    </WorkoutsContextProvider>
+  </React.StrictMode>,
+)
